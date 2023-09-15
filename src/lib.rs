591068909500ba87309431457b7c36623ee79e5d
LeonardoOReis <leonardooreis012@gmail.com>
2023-09-15 02:09:22 -0300

    new version

--- src/lib.rs
+++ src/lib.rs
@@ -1,104 +1,197 @@
+use rand::{distributions::Alphanumeric, Rng};
+use serde::{Deserialize, Serialize};
 use std::{
+    collections::HashSet,
     error::Error,
-    io::{Read, Write},
+    io::{self, Read, Write},
     net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream},
     str::FromStr,
-    sync::{Arc, RwLock},
-    thread,
-    time::Duration,
+    sync::mpsc::{self, Receiver, Sender},
+    thread::{self, JoinHandle},
+    time::{SystemTime, UNIX_EPOCH},
 };
 
 const PORT: u16 = 1024;
 
-pub struct Connection {
-    writer: Option<TcpStream>,
-    received_messages_size: usize,
-    received_messages: Arc<RwLock<String>>,
-    on_receive: Option<Box<dyn FnMut(&str)>>,
+pub struct Chat2 {
+    messages: Vec<Message>,
+    client: ChatClient,
+    client_rx: Receiver<String>,
+    input_rx: Receiver<String>,
 }
 
-impl Connection {
+impl Chat2 {
     pub fn new() -> Self {
-        let ip = local_ip_address::local_ip().unwrap();
-        println!("Local ip is {}", ip);
+        let ip = local_ip_address::local_ip();
+        println!("{}", ip.map_or_else(|a| a.to_string(), |b| b.to_string()));
 
-        let received_messages = Arc::new(RwLock::new(String::new()));
-        let received_messages_clone = received_messages.clone();
+        let (client_tx, client_rx) = mpsc::channel();
+        let (input_tx, input_rx) = mpsc::channel();
+
+        thread::spawn(move || loop {
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => input_tx.send(input).unwrap_or_else(|e| {
+                    eprintln!("Could not send input: {}", e);
+                }),
+                Err(e) => eprintln!("Could not read input: {}", e),
+            }
+        });
+        ChatServer::start_listening(client_tx);
+        Chat2 {
+            messages: Vec::new(),
+            client_rx,
+            input_rx,
+            client: ChatClient::new(),
+        }
+    }
+    pub fn looop(&mut self) {
+        loop {
+            self.client_rx.try_iter().for_each(|m| {
+                let m: Message = ron::from_str(&m).unwrap();
+                match m {
+                    Message::Disconnect(socket) => {
+                        self.client.receivers.remove(&socket);
+                    }
+                    Message::GetReceivers => {
+                        self.client
+                            .send_message(Message::PostReceivers(self.client.receivers.clone()));
+                    }
+                    Message::PostReceivers(r) => {
+                        self.client.receivers.extend(r.iter());
+                    }
+                    _ => {
+                        self.messages.push(m);
+                        println!("Messages is now: {:?}", self.messages);
+                    }
+                }
+            });
+            self.input_rx.try_iter().for_each(|i| {
+                if i.starts_with("!connect") {
+                    i.split(' ')
+                        .skip(1)
+                        .next()
+                        .and_then(|ip| self.client.add_receiver(ip).err())
+                        .and_then(|e| {
+                            eprintln!("Wrong parsing of ip: {}", e);
+                            Some(())
+                        });
+                    println!("List of receivers is now {:?}", self.client.receivers);
+                } else if i.starts_with("!disconnect") {
+                    let ip = local_ip_address::local_ip().unwrap();
+                    let ip = match ip {
+                        std::net::IpAddr::V4(i) => i,
+                        std::net::IpAddr::V6(_) => panic!("fuck you"),
+                    };
+                    self.client
+                        .send_message(Message::Disconnect(SocketAddrV4::new(ip, PORT)));
+                    self.client.clear_receivers();
+                    println!("List of receivers is now {:?}", self.client.receivers);
+                } else if i.starts_with("!setname") {
+                    i.split(' ').skip(1).next().and_then(|name| {
+                        self.client.set_user_name(name);
+                        Some(())
+                    });
+                    println!("Name is now {}", self.client.user_name);
+                } else {
+                    self.client.send_normal_message(i).unwrap_or_else(|e| {
+                        eprintln!("Could not send: {}", e);
+                    });
+                }
+            });
+        }
+    }
+}
+
+struct ChatServer;
+
+impl ChatServer {
+    fn start_listening(tx: Sender<String>) {
         thread::spawn(move || {
             println!("Listener thread started...");
             let listener = TcpListener::bind("0.0.0.0:1024").unwrap();
             for incoming in listener.incoming() {
                 if let Ok(mut stream) = incoming {
                     println!("Connection established");
-                    stream
-                        .set_read_timeout(Some(Duration::from_millis(50)))
-                        .expect("Could not unwrap TcpStream...");
-                    loop {
-                        if let Ok(mut d) = received_messages_clone.write() {
-                            match stream.read_to_string(&mut d) {
-                                Ok(u) => {
-                                    if u == 0 {
-                                        break;
-                                    }
-                                }
-
-                                Err(e) => (),
-                            }
-                        }
-                        thread::sleep(Duration::from_millis(50));
+                    let mut received = String::new();
+                    if let Ok(_) = stream.read_to_string(&mut received) {
+                        tx.send(received).unwrap_or_else(|e| {
+                            eprintln!("Could not send received string: {}", e);
+                        });
                     }
                     println!("Connection lost");
                 }
             }
         });
-
-        Connection {
-            writer: None,
-            on_receive: None,
-            received_messages_size: 0,
-            received_messages,
-        }
-    }
-    pub fn on_receive<T: FnMut(&str) + 'static>(&mut self, t: T) {
-        self.on_receive = Some(Box::new(t));
-    }
-    pub fn connect(&mut self, ip_address: &str) -> Result<(), Box<dyn Error>> {
-        let ip = Ipv4Addr::from_str(ip_address)?;
-        self.writer = Some(TcpStream::connect(SocketAddrV4::new(ip, PORT))?);
-        Ok(())
-    }
-    pub fn is_connected(&self) -> bool {
-        self.writer.is_some()
-    }
-    pub fn listen_loop(&mut self) -> Result<(), std::io::Error> {
-        if let Ok(r) = self.received_messages.try_read() {
-            r.lines().skip(self.received_messages_size).for_each(|x| {
-                self.received_messages_size += 1;
-                if let Some(on_receive) = self.on_receive.as_mut() {
-                    on_receive(x);
-                }
-            });
-        }
-        Ok(())
-    }
-    pub fn send(&mut self, message: &str) -> Result<(), Box<dyn Error>> {
-        if let Some(writer) = self.writer.as_mut() {
-            writer.write_all(message.as_bytes())?;
-            writer.flush()?;
-        }
-        Ok(())
-    }
-    pub fn disconnect(&mut self) -> Result<(), Box<dyn Error>> {
-        if let Some(writer) = self.writer.as_mut() {
-            writer.shutdown(std::net::Shutdown::Write)?;
-        }
-        self.writer = None;
-        Ok(())
     }
 }
 
-pub fn run() {
-    //c.on_receive(Callback).listen();
-    //c.send("Hello");
-    //c.disconnect();
+#[derive(Serialize, Deserialize, Debug)]
+enum Message {
+    Normal {
+        author: String,
+        content: String,
+        time_stamp: u64,
+    },
+    GetReceivers,
+    PostReceivers(HashSet<SocketAddrV4>),
+    Disconnect(SocketAddrV4),
+}
+
+struct ChatClient {
+    user_name: String,
+    receivers: HashSet<SocketAddrV4>,
+}
+
+impl ChatClient {
+    fn new() -> Self {
+        let user_name = format!(
+            "user {}",
+            rand::thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(6)
+                .map(char::from)
+                .collect::<String>()
+        );
+        ChatClient {
+            user_name,
+            receivers: HashSet::new(),
+        }
+    }
+    fn send_message(&self, message: Message) -> Result<(), Box<dyn Error>> {
+        self.send_raw(ron::to_string(&message)?)?;
+        Ok(())
+    }
+    fn send_normal_message(&self, message: String) -> Result<(), Box<dyn Error>> {
+        let now = SystemTime::now();
+        let time_stamp = now.duration_since(UNIX_EPOCH).unwrap().as_secs();
+        let message = Message::Normal {
+            time_stamp,
+            content: message,
+            author: self.user_name.clone(),
+        };
+        self.send_raw(ron::to_string(&message)?)?;
+        Ok(())
+    }
+    fn send_raw(&self, message: String) -> Result<(), Box<dyn Error>> {
+        for receiver in &self.receivers {
+            let mut stream = TcpStream::connect(receiver)?;
+            stream.write_all(message.as_bytes())?;
+            stream.flush()?;
+            stream.shutdown(std::net::Shutdown::Write)?;
+        }
+        Ok(())
+    }
+    fn set_user_name(&mut self, user_name: &str) {
+        self.user_name = String::from(user_name.trim());
+    }
+    fn add_receiver(&mut self, ip: &str) -> Result<(), Box<dyn Error>> {
+        self.receivers
+            .insert(SocketAddrV4::new(Ipv4Addr::from_str(ip.trim())?, PORT));
+        self.send_message(Message::GetReceivers)?;
+        Ok(())
+    }
+    fn clear_receivers(&mut self) {
+        self.receivers.clear();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,6 @@
-use std::{sync::mpsc, thread};
-
-use chat::Connection;
+use chat::Chat2;
 
 fn main() {
-    let mut c = Connection::new();
-    c.on_receive(|s| {
-        print!("Other used said \"{}\"", s);
-    });
-    let (tx, rx) = mpsc::channel::<String>();
-    thread::spawn(move || loop {
-        let mut input = String::new();
-        let _ = std::io::stdin()
-            .read_line(&mut input)
-            .map(|_| tx.send(input))
-            .map_err(|e| {
-                eprintln!("Something happened on the input thread: {}", e);
-                e
-            });
-    });
-    loop {
-        let _ = c.listen_loop();
-        if let Ok(r) = rx.try_recv() {
-            print!("Typed: {}", r);
-            if c.is_connected() {
-                println!("Trying to send...");
-                let _ = c.send(&r).map(|o| {
-                    println!("Sent message");
-                    o
-                }).map_err(|e| {
-                    eprintln!("Failure to send: {}", e);
-                    e
-                });
-            } else {
-                println!("Trying to connect to {}", r);
-                let _ = c.connect(r.trim()).map(|o| {
-                    println!("Connected");
-                    o
-                }).map_err(|e| {
-                    eprintln!("Failure to connect: {}", e);
-                    e
-                });
-            }
-        }
-    }
+    let mut c = Chat2::new();
+    c.looop();
 }
